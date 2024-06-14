db3571c3a8a56a51439289bec0e4f6cfe3e8ac23
nimsaysm <yasmin.soraya14@gmail.com>
2024-06-14 14:36:13 -0300

    Add: PhotoShouts

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_14_164844) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_14_171825) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "photo_shouts", force: :cascade do |t|
+    t.string "image_file_name", null: false
+    t.string "image_content_type", null: false
+    t.bigint "image_file_size", null: false
+    t.datetime "image_updated_at", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "shouts", force: :cascade do |t|
     t.bigint "user_id", null: false
