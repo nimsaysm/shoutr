6e2a6e8490b305c375a6b28de456e07038a35d0d
nimsaysm <yasmin.soraya14@gmail.com>
2024-06-17 12:04:04 -0300

    Add: Like and unlike Shouts

--- app/helpers/shout_helper.rb
+++ app/helpers/shout_helper.rb
@@ -1,3 +1,9 @@
 module ShoutHelper 
-
+    def like_button(shout)
+        if current_user.liked?(shout)
+            button_to "Unlike", unlike_shout_path(shout), method: :delete
+        else
+            button_to "Like", like_shout_path(shout), method: :post 
+        end
+    end
 end
